1f8d34f2f299e12a85f9806ca7c176265fd339df
Mátyás Vincze <53830475+matyasvincze98@users.noreply.github.com>
2021-09-15 15:39:10 +0200

    Update eval.py

--- eval.py
+++ eval.py
@@ -1,8 +1,7 @@
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
-
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
 
 
