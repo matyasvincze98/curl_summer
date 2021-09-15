2c0342f5ea3cb6bd0a65924fc35924da8aa191d8
Mátyás Vincze <53830475+matyasvincze98@users.noreply.github.com>
2021-09-15 15:35:26 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -15,6 +15,9 @@
 ################################################################################
 """Some common utils."""
 
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
