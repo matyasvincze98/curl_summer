dd2ccb4c46a0818fe2d163d7c5860d4692407c9d
Mátyás Vincze <53830475+matyasvincze98@users.noreply.github.com>
2021-09-15 15:36:54 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -15,14 +15,13 @@
 ################################################################################
 """Some common utils."""
 
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
 import logging
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 import tensorflow as tf
 import tensorflow_probability as tfp
 
