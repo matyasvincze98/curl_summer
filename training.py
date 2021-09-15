59d3438ca9bcd1e0a9f75a13bc1af49e901585bb
Mátyás Vincze <53830475+matyasvincze98@users.noreply.github.com>
2021-09-15 15:47:27 +0200

    Update training.py

--- training.py
+++ training.py
@@ -22,8 +22,6 @@ import logging
 import numpy as np
 import sonnet as snt
 
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
 import tensorflow_datasets as tfds
 import tensorflow_probability as tfp
