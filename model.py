1aab57c2e42599c655d3d55bea2c9d85af95cad0
Mátyás Vincze <53830475+matyasvincze98@users.noreply.github.com>
2021-10-11 10:06:58 +0200

    Update model.py

--- model.py
+++ model.py
@@ -35,8 +35,8 @@ class Lambda_VMatyas(snt.Module):
     
   # @snt.once
   if not hasattr(self, 'w'):
-    lambda_init = lambda s, dtype, partition_info: 
-    tf.random_uniform(shape=s, dtype=dtype, minval=0.1, maxval=1.0)
+    lambda_init = lambda s, dtype, partition_info: \
+              tf.random_uniform(shape=s, dtype=dtype, minval=0.1, maxval=1.0)
     self.w = tf.Variable(lambda_init, shape=[128,], name="w")
   return x * self.w
 
