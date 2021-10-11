3946f980cdace24765dbf4e79c65cfafe9384b27
Mátyás Vincze <53830475+matyasvincze98@users.noreply.github.com>
2021-10-11 10:08:21 +0200

    Update model.py

--- model.py
+++ model.py
@@ -38,7 +38,9 @@ class Lambda_VMatyas(snt.Module):
     lambda_init = lambda s, dtype, partition_info: \
               tf.random_uniform(shape=s, dtype=dtype, minval=0.1, maxval=1.0)
     self.w = tf.Variable(lambda_init, shape=[128,], name="w")
-  return x * self.w
+    
+  def __call__(self, x):
+    return x * self.w
 
 
 class SharedEncoder(snt.AbstractModule):
